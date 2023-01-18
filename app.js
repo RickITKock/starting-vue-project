6aaf00995006adba44cc7ef3109bc94028e06294
rick124 <rick@roosh.online>
2023-01-18 19:12:31 +0100

    Chore: add link and use v-bind:href

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "Finish the course and learn Vue!",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+}).mount("#user-goal");
