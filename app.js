bf98d6bdb2858f2b2273b0af393e48ac2eb6524b
rick124 <rick@roosh.online>
2023-01-18 20:04:35 +0100

    Chore: using the native event object

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    add() {
+      this.counter++;
+    },
+    reduce() {
+      this.counter--;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
