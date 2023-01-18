b1b77a8ed542f0f331fe384070036f13f0776c2e
rick124 <rick@roosh.online>
2023-01-18 19:23:52 +0100

    Chore: working with data inside Vue App

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoal: "Finish the course and learn Vue!",
+      courseGoalB: "Master the course!",
       vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber > 0.5) {
+        return this.courseGoal;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 }).mount("#user-goal");
