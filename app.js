b142222f498642752698786dd8cbb7e5122477d4
rick124 <rick@roosh.online>
2023-01-22 16:29:24 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
-    };
+    return { goals: [] };
   },
-  computed: {
-    boxAClasses() {
-      return { active: this.boxASelected };
-    },
-  },
-  methods: {
-    boxSelected(box) {
-      if (box === "A") {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === "B") {
-        this.boxBSelected = !this.boxBSelected;
-      } else if (box === "C") {
-        this.boxCSelected = !this.boxCSelected;
-      }
-    },
-  },
-}).mount("#styling");
+});
+
+app.mount('#user-goals');
