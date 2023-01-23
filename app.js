4b12ad4c6a07175f811a541b441f8461f97a0587
rick124 <rick@roosh.online>
2023-01-23 12:14:24 +0100

    log entire lifecycle

--- app.js
+++ app.js
@@ -15,9 +15,37 @@ const app = Vue.createApp({
     },
     setMessage() {
       this.message = this.$refs.message.value;
-      console.dir(this.$refs.message);
+      // console.dir(this.$refs.message);
     },
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
   },
 });
 
 app.mount("#user-goals");
+
+setTimeout(() => {
+  app.unmount();
+}, 3000);
